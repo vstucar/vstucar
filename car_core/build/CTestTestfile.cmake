d475afd31fb568d51121b8dfd25f1b0beb43d6dc
Markov Alexey <markovalex95@gmail.com>
2019-02-11 08:33:29 +0300

    Create and setup car_core and car_msgs packages

---
+++ car_core/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/garrus/ros/src/vstucar/car_core
+# Build directory: /home/garrus/ros/src/vstucar/car_core/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
