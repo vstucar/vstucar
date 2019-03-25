d0a72bfc9a7bea619831214992b60ac0c633d7f3
Markov Alexey <markovalex95@gmail.com>
2019-03-25 22:05:16 +0300

    Update .gitignore & delete files

--- car_core/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/garrus/ros/src/vstucar/car_core
-# Build directory: /home/garrus/ros/src/vstucar/car_core/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
